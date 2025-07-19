4ad44473c7de737090438005898e449137f78f0c
Taxi_tabby <rkdmf0000@gmail.com>
2025-07-20 00:14:04 +0900

    feat: add user edit functionality and enhance user list display

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { Admin, Resource, List, Datagrid, TextField } from "react-admin";
+import { Admin, Resource, List, Datagrid, TextField, Edit, SimpleForm, TextInput, EditButton, required } from "react-admin";
 import { Layout } from "./Layout";
 import { dataProvider } from "./dataProvider";
 import { authProvider } from "./authProvider";
+import { People } from "@mui/icons-material";
 
 const UserList = () => (
   <List>
@@ -9,9 +10,24 @@ const UserList = () => (
       <TextField source="id" />
       <TextField source="username" />
       <TextField source="fullName" />
+      <EditButton />
     </Datagrid>
   </List>
 );
+
+const UserEdit = () => (
+  <Edit>
+    <SimpleForm>
+      <TextInput source="id" disabled label="사용자 ID" />
+      <TextInput source="username" label="사용자명" validate={required()} />
+      <TextInput source="fullName" label="전체 이름" validate={required()} />
+      <TextInput source="password" type="password" label="비밀번호" />
+    </SimpleForm>
+  </Edit>
+);
+
+
+
 
 export const App = () => (
   <>
@@ -21,7 +37,13 @@ export const App = () => (
       authProvider={authProvider}
     >
       {/* Add your resources here */}
-      <Resource name="users" list={UserList} />
+      <Resource 
+        name="users" 
+        list={UserList}
+        edit={UserEdit}
+        icon={People}
+        options={{ label: '사용자 관리' }}
+      />
 
     </Admin>
 
